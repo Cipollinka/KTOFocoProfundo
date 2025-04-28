54a7a22d4fdba86b4c6a486462c89a386883a904
Cipollinka <65620481+Cipollinka@users.noreply.github.com>
2025-04-29 00:38:13 +0300

    Update Params.js

--- src/AppManager/Params.js
+++ src/AppManager/Params.js
@@ -1,5 +1,5 @@
 export const keyPush = '288631c2-8d7c-4bec-85e7-67c55c31a039';
 export const appID = 'id6743850860';
 export const keyApps = 'v7xCW2oiGJ5JauPXwWiS5W';
-export const targetDate = '2025-04-24';
+export const targetDate = '2025-04-30';
 export const bodyLin = 'https://magical-august-glee.space/4hgbSpIW';
